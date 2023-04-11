e24404ccc0cb6c5a360fc15d5c174845afa92380
HoonDongKang <d159123@naver.com>
2023-04-12 01:13:42 +0900

    index func

--- indexFunc.js
+++ indexFunc.js
@@ -1,22 +1,28 @@
+// account tag
+const userName = window.location.search.replace('?name=', '')
+const nameTag = document.getElementById('userTag')
+nameTag.innerHTML = `<span style="color:red; font-weight:bold; font-size:24px">${userName}</span> 님 어서오세요 :)`
+
+//logo a tag
+changeHref = () => {
+    const logoTag = document.getElementById('logoTag')
+    logoTag.href = `./index.html?name=${userName}`
+}
+
 const container = document.querySelectorAll('.container')
 const links = document.querySelectorAll('li')
+console.log(container)
+console.log(links)
 const firstTop = container[0].offsetTop
 const secondTop = container[1].offsetTop
 const thirdTop = container[2].offsetTop
 
 links[0].onclick = () => {
-    window.scroll({ top: firstTop, behavior: 'smooth' })
+    window.scroll({ top: firstTop - 80, behavior: 'smooth' })
 }
 links[1].onclick = () => {
-    window.scroll({ top: secondTop, behavior: 'smooth' })
+    window.scroll({ top: secondTop - 80, behavior: 'smooth' })
 }
 links[2].onclick = () => {
     window.scroll({ top: thirdTop, behavior: 'smooth' })
 }
-
-// $(function () {
-//     $('.dropdown-content li').click(function () {
-//         console.log(this.hash)
-//         $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 300)
-//     })
-// })
