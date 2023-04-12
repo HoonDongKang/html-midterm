fad2df6f54f702fe01ccad51170f6c9876954ed5
HoonDongKang <d159123@naver.com>
2023-04-12 22:09:55 +0900

    update ftp

--- indexFunc.js
+++ indexFunc.js
@@ -1,28 +1,23 @@
 // account tag
-const userName = window.location.search.replace('?name=', '')
-const nameTag = document.getElementById('userTag')
-nameTag.innerHTML = `<span style="color:red; font-weight:bold; font-size:24px">${userName}</span> 님 어서오세요 :)`
+const userName = window.location.search.replace("?name=", "");
+const nameTag = document.getElementById("userTag");
+nameTag.innerHTML = `<span style="color:red; font-weight:bold; font-size:24px">${userName}</span> 님 어서오세요 :)`;
 
 //logo a tag
 changeHref = () => {
-    const logoTag = document.getElementById('logoTag')
-    logoTag.href = `./index.html?name=${userName}`
-}
+  const logoTag = document.getElementById("logoTag");
+  logoTag.href = `./home.html?name=${userName}`;
+};
 
-const container = document.querySelectorAll('.container')
-const links = document.querySelectorAll('li')
-console.log(container)
-console.log(links)
-const firstTop = container[0].offsetTop
-const secondTop = container[1].offsetTop
-const thirdTop = container[2].offsetTop
+const containerList = document.querySelectorAll(".container");
+const links = document.querySelectorAll("li");
+let containerOffsetTopList = [];
 
-links[0].onclick = () => {
-    window.scroll({ top: firstTop - 80, behavior: 'smooth' })
+for (let container of containerList) {
+  containerOffsetTopList.push(container.offsetTop);
 }
-links[1].onclick = () => {
-    window.scroll({ top: secondTop - 80, behavior: 'smooth' })
-}
-links[2].onclick = () => {
-    window.scroll({ top: thirdTop, behavior: 'smooth' })
+for (let i = 0; i < links.length; i++) {
+  links[i].onclick = () => {
+    window.scroll({ top: containerOffsetTopList[i] - 80, behavior: "smooth" });
+  };
 }
--- loginFunc.js
+++ loginFunc.js
@@ -1,49 +1,49 @@
 getShow = () => {
-    const firstDiv = document.getElementById('firstFormDiv')
-    const secondDiv = document.getElementById('secondFormDiv')
+  const firstDiv = document.getElementById("firstFormDiv");
+  const secondDiv = document.getElementById("secondFormDiv");
 
-    if (firstDiv.style.display === 'block') {
-        firstDiv.style.display = 'none'
-        secondDiv.style.display = 'block'
-    } else {
-        firstDiv.style.display = 'block'
-        secondDiv.style.display = 'none'
-    }
-}
+  if (firstDiv.style.display === "block") {
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "block";
+  } else {
+    firstDiv.style.display = "block";
+    secondDiv.style.display = "none";
+  }
+};
 
 checkPw = () => {
-    const checkMsg = document.getElementById('pwMsg')
-    const password = document.getElementById('userPw')
-    const passwordCheck = document.getElementById('userPwC')
-    if (password.value === passwordCheck.value) {
-        checkMsg.innerHTML = '비밀번호가 일치합니다.'
-        checkMsg.style.color = 'green'
-    } else {
-        checkMsg.innerHTML = '비밀번호가 일치하지 않습니다.'
-        checkMsg.style.color = 'red'
-    }
-}
+  const checkMsg = document.getElementById("pwMsg");
+  const password = document.getElementById("userPw");
+  const passwordCheck = document.getElementById("userPwC");
+  if (password.value === passwordCheck.value) {
+    checkMsg.innerHTML = "비밀번호가 일치합니다.";
+    checkMsg.style.color = "green";
+  } else {
+    checkMsg.innerHTML = "비밀번호가 일치하지 않습니다.";
+    checkMsg.style.color = "red";
+  }
+};
 
 submitForm = (e) => {
-    const id = document.getElementById('userId')
-    const email = document.getElementById('userEmail')
-    const password = document.getElementById('userPw')
-    const name = document.getElementById('userName')
-    const number = document.getElementById('userNumber')
-    const gender = document.getElementsByName('gender')
-    let checkedGender
-    const country = document.getElementById('country')
+  const id = document.getElementById("userId");
+  const email = document.getElementById("userEmail");
+  const password = document.getElementById("userPw");
+  const name = document.getElementById("userName");
+  const number = document.getElementById("userNumber");
+  const gender = document.getElementsByName("gender");
+  let checkedGender;
+  const country = document.getElementById("country");
 
-    for (const input of gender) {
-        if (input.checked) {
-            checkedGender = input.value
-            break
-        }
+  for (const input of gender) {
+    if (input.checked) {
+      checkedGender = input.value;
+      break;
     }
+  }
 
-    alert(`안녕하세요 ${name.value}님
+  alert(`안녕하세요 ${name.value}님
 당신의 아이디는 ${id.value}고 비밀번호는 ${password.value}이군요.
 당신은 ${checkedGender}이며 국적은 ${country.value}네요.
-앞으로 당신에게 연락을 드릴 때 ${number.value} 또는 ${email.value}로 연락드릴게요.`)
-    window.location = `./index.html?name=${name.value}`
-}
+앞으로 당신에게 연락을 드릴 때 ${number.value} 또는 ${email.value}로 연락드릴게요.`);
+  window.location = `./home.html?name=${name.value}`;
+};
