769da17538b28ee6fb632c9c88429c17b654b55f
HoonDongKang <d159123@naver.com>
2023-04-15 13:03:48 +0900

    update

--- indexFunc.js
+++ indexFunc.js
@@ -1,23 +1,24 @@
 // account tag
-const userName = window.location.search.replace("?name=", "");
-const nameTag = document.getElementById("userTag");
-nameTag.innerHTML = `<span style="color:red; font-weight:bold; font-size:24px">${userName}</span> 님 어서오세요 :)`;
+const userName = window.location.search.replace('?name=', '')
+const nameTag = document.getElementById('userTag')
+const decodedName = decodeURI(decodeURIComponent(userName))
+nameTag.innerHTML = `<span style="color:red; font-weight:bold; font-size:24px">${decodedName}</span> 님 어서오세요 :)`
 
 //logo a tag
 changeHref = () => {
-  const logoTag = document.getElementById("logoTag");
-  logoTag.href = `./home.html?name=${userName}`;
-};
+  const logoTag = document.getElementById('logoTag')
+  logoTag.href = `./home.html?name=${userName}`
+}
 
-const containerList = document.querySelectorAll(".container");
-const links = document.querySelectorAll("li");
-let containerOffsetTopList = [];
+const containerList = document.querySelectorAll('.container')
+const links = document.querySelectorAll('li')
+let containerOffsetTopList = []
 
 for (let container of containerList) {
-  containerOffsetTopList.push(container.offsetTop);
+  containerOffsetTopList.push(container.offsetTop)
 }
 for (let i = 0; i < links.length; i++) {
   links[i].onclick = () => {
-    window.scroll({ top: containerOffsetTopList[i] - 80, behavior: "smooth" });
-  };
+    window.scroll({ top: containerOffsetTopList[i] - 80, behavior: 'smooth' })
+  }
 }
