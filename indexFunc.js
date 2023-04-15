58c4b678ec780e69ef6af46194ff26d61a5f71d1
HoonDongKang <d159123@naver.com>
2023-04-16 00:06:09 +0900

    floating menu

--- indexFunc.js
+++ indexFunc.js
@@ -5,20 +5,29 @@ const decodedName = decodeURI(decodeURIComponent(userName))
 nameTag.innerHTML = `<span style="color:red; font-weight:bold; font-size:24px">${decodedName}</span> 님 어서오세요 :)`
 
 //logo a tag
-changeHref = () => {
-  const logoTag = document.getElementById('logoTag')
-  logoTag.href = `./home.html?name=${userName}`
+logoHref = () => {
+    const logoTag = document.getElementById('logoTag')
+    console.log(logoTag)
+    logoTag.href = `./home.html?name=${userName}`
 }
 
+aboutMeHref = () => {
+    const aboutMeBtn = document.getElementById('AboutMebtn')
+    console.log(aboutMeBtn)
+    aboutMeBtn.href = `./home.html?name=${userName}`
+}
 const containerList = document.querySelectorAll('.container')
 const links = document.querySelectorAll('li')
 let containerOffsetTopList = []
 
 for (let container of containerList) {
-  containerOffsetTopList.push(container.offsetTop)
+    containerOffsetTopList.push(container.offsetTop)
 }
 for (let i = 0; i < links.length; i++) {
-  links[i].onclick = () => {
-    window.scroll({ top: containerOffsetTopList[i] - 80, behavior: 'smooth' })
-  }
+    links[i].onclick = () => {
+        window.scroll({
+            top: containerOffsetTopList[i] - 200,
+            behavior: 'smooth',
+        })
+    }
 }
