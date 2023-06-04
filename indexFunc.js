f41cb82e36f02345dde2515efa7b773ae4b1dcf6
HoonDongKang <d159123@naver.com>
2023-06-04 18:04:49 +0900

    update

--- indexFunc.js
+++ indexFunc.js
@@ -36,3 +36,75 @@ getPrompt = () => {
     const promptValue = prompt('Prompt 버튼을 누루셨습니다.')
     promptSpan.innerHTML = promptValue
 }
+
+ifElse = (value) => {
+    console.log(value)
+    let result = document.getElementById('ifElse')
+    if (!value) {
+        result.innerHTML = '값을 적어주세요.'
+    } else if (value <= 10) {
+        result.innerHTML = '값이 10이상입니다.'
+    } else if (value <= 100) {
+        result.innerHTML = '값이 10이상이고 100이하입니다.'
+    } else {
+        result.innerHTML = '값이 100보다 큽니다.'
+    }
+}
+swtichCase = (value) => {
+    let result = document.getElementById('switch')
+
+    switch (value) {
+        case '사과':
+            result.innerHTML = 200
+            break
+        case '포도':
+            result.innerHTML = 300
+            break
+        case '체리':
+            result.innerHTML = 400
+            break
+        default:
+            result.innerHTML = '사과, 포도, 체리 중 하나를 적어주세요'
+    }
+}
+
+forCase = (value) => {
+    let result = document.getElementById('forResult')
+    result.innerHTML = ''
+    let forArr = []
+    if (value > 10) {
+        result.innerHTML = '10 이하의 수를 적으세요.'
+    } else {
+        for (let i = 0; i < Number(value); i++) {
+            result.innerHTML += i + '  '
+        }
+    }
+}
+
+whileCase = (value) => {
+    let result = document.getElementById('whileResult')
+    result.innerHTML = ''
+    let i = Number(value)
+    if (value > 10) {
+        result.innerHTML = '10 이하의 수를 적으세요.'
+    } else {
+        while (i > 0) {
+            result.innerHTML += i + '  '
+            i--
+        }
+    }
+}
+dowhileCase = (value) => {
+    let result = document.getElementById('dowhileResult')
+    result.innerHTML = ''
+    let i = Number(value)
+    if (value > 10) {
+        result.innerHTML = '10 이하의 수를 적으세요.'
+    } else {
+        do {
+            result.innerHTML = 'do 문장 실행' + ' '
+            result.innerHTML += i + '  '
+            i--
+        } while (i < 0)
+    }
+}
