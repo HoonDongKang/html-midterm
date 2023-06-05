5a632c3e5f94854a3b7f81e44879eb70b75c7451
HoonDongKang <d159123@naver.com>
2023-06-05 09:49:47 +0900

    finish js

--- indexFunc.js
+++ indexFunc.js
@@ -10,101 +10,100 @@ const links = document.querySelectorAll('.floating')
 let containerOffsetTopList = []
 
 for (let container of containerList) {
-    containerOffsetTopList.push(container.offsetTop)
+  containerOffsetTopList.push(container.offsetTop)
 }
 for (let i = 0; i < links.length; i++) {
-    links[i].onclick = () => {
-        window.scroll({
-            top: containerOffsetTopList[i] - 200,
-            behavior: 'smooth',
-        })
-    }
+  links[i].onclick = () => {
+    window.scroll({
+      top: containerOffsetTopList[i] - 200,
+      behavior: 'smooth',
+    })
+  }
 }
 
 getAlert = () => {
-    const alertSpan = document.getElementById('alertValue')
-    const alertValue = alert('Alert 버튼을 누루셨습니다.')
-    alertSpan.innerHTML = alertValue
+  const alertSpan = document.getElementById('alertValue')
+  const alertValue = alert('Alert 버튼을 누루셨습니다.')
+  alertSpan.innerHTML = alertValue
 }
 getConfirm = () => {
-    const confirmSpan = document.getElementById('confirmValue')
-    const confirmValue = confirm('Confirm 버튼을 누루셨습니다.')
-    confirmSpan.innerHTML = confirmValue
+  const confirmSpan = document.getElementById('confirmValue')
+  const confirmValue = confirm('Confirm 버튼을 누루셨습니다.')
+  confirmSpan.innerHTML = confirmValue
 }
 getPrompt = () => {
-    const promptSpan = document.getElementById('promptValue')
-    const promptValue = prompt('Prompt 버튼을 누루셨습니다.')
-    promptSpan.innerHTML = promptValue
+  const promptSpan = document.getElementById('promptValue')
+  const promptValue = prompt('Prompt 버튼을 누루셨습니다.')
+  promptSpan.innerHTML = promptValue
 }
 
 ifElse = (value) => {
-    console.log(value)
-    let result = document.getElementById('ifElse')
-    if (!value) {
-        result.innerHTML = '값을 적어주세요.'
-    } else if (value <= 10) {
-        result.innerHTML = '값이 10이상입니다.'
-    } else if (value <= 100) {
-        result.innerHTML = '값이 10이상이고 100이하입니다.'
-    } else {
-        result.innerHTML = '값이 100보다 큽니다.'
-    }
+  console.log(value)
+  let result = document.getElementById('ifElse')
+  if (!value) {
+    result.innerHTML = '값을 적어주세요.'
+  } else if (value <= 10) {
+    result.innerHTML = '값이 10이상입니다.'
+  } else if (value <= 100) {
+    result.innerHTML = '값이 10이상이고 100이하입니다.'
+  } else {
+    result.innerHTML = '값이 100보다 큽니다.'
+  }
 }
 swtichCase = (value) => {
-    let result = document.getElementById('switch')
+  let result = document.getElementById('switch')
 
-    switch (value) {
-        case '사과':
-            result.innerHTML = 200
-            break
-        case '포도':
-            result.innerHTML = 300
-            break
-        case '체리':
-            result.innerHTML = 400
-            break
-        default:
-            result.innerHTML = '사과, 포도, 체리 중 하나를 적어주세요'
-    }
+  switch (value) {
+    case '사과':
+      result.innerHTML = 200
+      break
+    case '포도':
+      result.innerHTML = 300
+      break
+    case '체리':
+      result.innerHTML = 400
+      break
+    default:
+      result.innerHTML = '사과, 포도, 체리 중 하나를 적어주세요'
+  }
 }
 
 forCase = (value) => {
-    let result = document.getElementById('forResult')
-    result.innerHTML = ''
-    let forArr = []
-    if (value > 10) {
-        result.innerHTML = '10 이하의 수를 적으세요.'
-    } else {
-        for (let i = 0; i < Number(value); i++) {
-            result.innerHTML += i + '  '
-        }
+  let result = document.getElementById('forResult')
+  result.innerHTML = ''
+  let forArr = []
+  if (value > 10) {
+    result.innerHTML = '10 이하의 수를 적으세요.'
+  } else {
+    for (let i = 0; i < Number(value); i++) {
+      result.innerHTML += i + '  '
     }
+  }
 }
 
 whileCase = (value) => {
-    let result = document.getElementById('whileResult')
-    result.innerHTML = ''
-    let i = Number(value)
-    if (value > 10) {
-        result.innerHTML = '10 이하의 수를 적으세요.'
-    } else {
-        while (i > 0) {
-            result.innerHTML += i + '  '
-            i--
-        }
+  let result = document.getElementById('whileResult')
+  result.innerHTML = ''
+  let i = Number(value)
+  if (value > 10) {
+    result.innerHTML = '10 이하의 수를 적으세요.'
+  } else {
+    while (i > 0) {
+      result.innerHTML += i + '  '
+      i--
     }
+  }
 }
 dowhileCase = (value) => {
-    let result = document.getElementById('dowhileResult')
-    result.innerHTML = ''
-    let i = Number(value)
-    if (value > 10) {
-        result.innerHTML = '10 이하의 수를 적으세요.'
-    } else {
-        do {
-            result.innerHTML = 'do 문장 실행' + ' '
-            result.innerHTML += i + '  '
-            i--
-        } while (i < 0)
-    }
+  let result = document.getElementById('dowhileResult')
+  result.innerHTML = ''
+  let i = Number(value)
+  if (value > 10) {
+    result.innerHTML = '10 이하의 수를 적으세요.'
+  } else {
+    do {
+      result.innerHTML += i + ' '
+      i--
+    } while (i > 0)
+  }
 }
